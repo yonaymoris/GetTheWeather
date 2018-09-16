e9db766df2dfce0b2b3c745a6181a1128da88048
Phuong <ngompung@gmail.com>
2018-09-16 22:32:33 +0300

    Initial Commit

---
+++ GetTheWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetTheWeather
+//
+//  Created by Phuong Ngo on 9/16/18.
+//  Copyright © 2018 Phuong Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
